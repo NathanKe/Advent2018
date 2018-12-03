2c334fd2e7daea770fea2529d334f93091f74893
Nathan Kearney <nkearney89@gmail.com>
2018-12-03 00:49:59 -0500

    D3 part 1

--- 03-answer.ps1
+++ 03-answer.ps1
@@ -1,1 +1,34 @@
 $in = gc .\03-input.txt
+
+#Part 1
+
+$hash = @{}
+
+$in | % {
+	$curClaim = $_
+	
+	if($curClaim -match "^.(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)$"){
+		$id = +$matches[1]
+		$sx = +$matches[2]
+		$sy = +$matches[3]
+		$lx = +$matches[4]
+		$ly = +$matches[5]
+		
+		
+		($sx..($sx+$lx-1)) | % {
+			$cx = $_
+			($sy..($sy+$ly-1)) | % {
+				$cy = $_
+				
+				$check = $hash["$cx : $cy"]
+				if($check){
+					$hash["$cx : $cy"]++
+				}else{
+					$hash.add("$cx : $cy",1)
+				}
+			}
+		}
+	}
+}
+
+($hash.getEnumerator() | ? {$_.value -ne 1}).length
