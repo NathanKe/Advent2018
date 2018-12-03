01d25f3a9691ba2a7dae21e766ddac4b7eb55f20
Nathan Kearney <kearneyn@brynmawrschool.org>
2018-12-03 16:02:14 -0500

    D3P2 Complete

--- 03-answer.ps1
+++ 03-answer.ps1
@@ -1,9 +1,14 @@
 $in = gc .\03-input.txt
 
+#$in = @("#1 @ 1,3: 4x4","#2 @ 3,1: 4x4","#3 @ 5,5: 2x2")
+
+
 #Part 1
+
 
 $hash = @{}
 
+Write-Host "expanding"
 $in | % {
 	$curClaim = $_
 	
@@ -17,18 +22,36 @@ $in | % {
 		
 		($sx..($sx+$lx-1)) | % {
 			$cx = $_
+			if(!$hash[$cx]){
+				$hash.add($cx,@{})
+			}
 			($sy..($sy+$ly-1)) | % {
 				$cy = $_
-				
-				$check = $hash["$cx : $cy"]
-				if($check){
-					$hash["$cx : $cy"]++
-				}else{
-					$hash.add("$cx : $cy",1)
+				if(!$hash[$cx][$cy]){
+					$hash[$cx].add($cy,[pscustomobject]@{ids=(new-object system.collections.arraylist);hits=0})
 				}
+				
+				$hash[$cx][$cy].hits++
+				$hash[$cx][$cy].ids.add($id)>$null
 			}
 		}
 	}
 }
 
-($hash.getEnumerator() | ? {$_.value -ne 1}).length
+
+$expand = $hash.values.values
+
+Write-Host "Counting Overlap"
+($expand.hits | ? {$_ -gt 1}).count
+
+Write-Host "MultiClaims"
+$multiClaims = $expand | ? {$_.ids.count -ne 1}
+Write-Host "Id Expansion"
+$idsMultiClaims = $multiClaims | select -expandproperty ids
+Write-Host "Unique IDs"
+$multiClaimsReduce = $idsMultiClaims | group | select -expandproperty name
+Write-Host "CrossReference"
+$nonMultiClaims = 1..$in.count |? {$_ -notin $multiClaimsReduce}
+Write-Host "Singleton"
+$nonMultiClaims
+
